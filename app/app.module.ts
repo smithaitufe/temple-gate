6392502de287ace81bcf507cd4b08165f8c402e7
Smith Aitufe <smithaitufe@live.com>
2016-11-26 17:39:23 +0100

    Corrections to app.routing and app.module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { appRouting, appComponents } from './app.routes';
+import { appRouting, appComponents } from './app.routing';
 
 
 @NgModule({
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,4 +1,3 @@
-import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';

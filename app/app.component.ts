e22122ae9cb226c2c7e8a24d1378b4081eeee52f
Smith Aitufe <smithaitufe@live.com>
2016-11-26 23:10:31 +0100

    Used templateUrl in app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,21 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-    <header>
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/">Temple Gate</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li><a routerLink='/about'>About</a></li>
-                    <li><a routerLink='/contact'>Contact</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <router-outlet></router-outlet>
-    `
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent {}

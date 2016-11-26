57aa9972e302b6b2a276e5d60d2fb948c14bb3ad
Smith Aitufe <smithaitufe@live.com>
2016-11-26 17:11:20 +0100

    Added the header and navbar section to app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<router-outlet></router-outlet>`
+    template: `
+    <header>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/">Temple Gate</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a [routerLink]=['about']>About</a></li>
+                    <li><a [routerLink]=['contact']>Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <router-outlet></router-outlet>`
 })
 export class AppComponent {}
